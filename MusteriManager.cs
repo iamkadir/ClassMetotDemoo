d4fdf6bb49fd1a2bc3f3f872d1b07606c8065f8a
Kadir Kaya <66167379+iamkadir@users.noreply.github.com>
2021-02-08 00:04:32 +0300

    Add files via upload

---
+++ MusteriManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkle(int Id, string Adi, string Soyadı , string Sehir)
+        {
+            Console.WriteLine(Id + " - " + Adi + " - " + Soyadı + " - " + Sehir );
+        }
+
+        public void Ekle (Musteri musteri)
+        {
+            Console.WriteLine( "Bilgiler: " +  musteri.Id + " - " +  musteri.Adi +" - " + musteri.Soyadı +" - " +musteri.Sehir);
+        }
+        
+        public void Sil (Musteri musteri)
+        {
+            Console.WriteLine("Silinecek Kişinin Id'si: 35 ");
+        }
+
+    }
+}
